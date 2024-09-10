47ed27a2a4f32ffa85cfc0cefe5f5f0c8fb6ff8a
alisher1129 <ali.sher@vaival.com>
2024-09-10 19:34:59 +0500

    completed

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+ 
+  @PrimaryGeneratedColumn()
+  id: number;
+ 
+  @Column()
+  firstName: string;
+ 
+  @Column()
+  lastName: string;
+ 
+  @Column()
+  age: number;
+}
