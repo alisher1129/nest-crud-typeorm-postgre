3bcd59674df95a6f496e15af4c1f43718813ac40
alisher1129 <ali.sher@vaival.com>
2024-09-10 15:16:50 +0500

    connect DB and setup user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'alisher1',
+      password: '12345', 
+      database: 'TypeORM-DB',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule],
+  controllers: [AppController ],
   providers: [AppService],
 })
 export class AppModule {}
